9cba62d998279c74346f52b3585f642d40e66494
boris-civiplanner <boris@civilplanner.co>
2025-03-12 16:57:26 +0200

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from 'firebase/auth';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDe5Z5cVhreXu9a95vBFvlCt-5rmYQpdUo",
-  authDomain: "civil-planner-demo.firebaseapp.com",
-  projectId: "civil-planner-demo",
-  storageBucket: "civil-planner-demo.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef123456789012345"
+  apiKey: "AIzaSyB5STm_sxcJ-ESsGrdI_Vj7rjZib07P28A",
+  authDomain: "civilplanner-49fcc.firebaseapp.com",
+  projectId: "civilplanner-49fcc",
+  storageBucket: "civilplanner-49fcc.firebasestorage.app",
+  messagingSenderId: "915978277757",
+  appId: "1:915978277757:web:9bbf886765fbca75a3b214",
+  measurementId: "G-ZP1X53YW6P"
 };
 
 // Initialize Firebase
